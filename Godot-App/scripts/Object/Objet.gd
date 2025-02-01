423abc1a2c6eae562defe1b4d206ff0e57fc5712
Nantenaina isaac <ujesaisnantah@gmail.com>
2025-02-01 12:31:24 +0300

    update

--- Godot-App/scripts/Object/Objet.gd
+++ Godot-App/scripts/Object/Objet.gd
@@ -1,20 +1,28 @@
 extends Node3D
 
-# Variable pour savoir si le joueur est à proximité
 var is_player_near = false
 
 func _ready():
-	# Ajoute l'objet au groupe "interactable"
 	add_to_group("interactable")
 
 func _on_area_entered(area):
-	# Vérifie si le joueur est entré dans la zone
 	if area.is_in_group("player"):
 		is_player_near = true
 		print("Joueur proche de l'objet")
+		can_take_it()
 
 func _on_area_exited(area):
-	# Vérifie si le joueur est sorti de la zone
 	if area.is_in_group("player"):
 		is_player_near = false
 		print("Joueur n'est plus proche de l'objet")
+		cant_take_it()
+
+func can_take_it():
+	if GameManager.player:
+		GameManager.player.cane_take_object = true
+		GameManager.player.nearby_object = self
+
+func cant_take_it():
+	if GameManager.player:
+		GameManager.player.cane_take_object = false
+		GameManager.player.nearby_object = null
--- Godot-App/scripts/player/player.gd
+++ Godot-App/scripts/player/player.gd
@@ -2,10 +2,10 @@ extends CharacterBody3D
 
 const SPEED = 2.0
 const JUMP_VELOCITY = 4.5
+const ROTATION_SPEED = 10.0
 
 @onready var camera_point = $camera_point
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var animation_player = $Visuals/player/AnimationPlayer
@@ -13,8 +13,9 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var walking = false
 
-# Variable pour stocker l'objet proche
 var nearby_object: Node3D = null
+
+var cane_take_object = false
 
 func _ready():
 	GameManager.set_player(self)
@@ -22,22 +23,27 @@ func _ready():
 	animation_player.set_blend_time("walk", "idle", 0.2)
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		
-		visuals.look_at(direction + position)
+		var iso_direction = Vector3(
+		-direction.x + direction.z,
+		0,
+		-direction.x - direction.z
+		).normalized()
+		
+		var target_angle = atan2(iso_direction.x, iso_direction.z)
+		visuals.rotation.y = lerp_angle(visuals.rotation.y, target_angle, ROTATION_SPEED * delta)
+		
 		if !walking:
 			walking = true
 			animation_player.play("walk")
@@ -50,28 +56,10 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-	# Gestion de l'interaction avec l'objet
-	if Input.is_action_just_pressed("interact") and nearby_object != null:
+	if Input.is_action_just_pressed("interact") and cane_take_object:
 		pick_up_object(nearby_object)
 
-func _on_area_entered(area):
-	# Vérifie si l'objet entrant est un objet interactif (comme un Plat)
-	if area.is_in_group("interactable"):
-		nearby_object = area.get_parent()  # Stocke l'objet proche
-		print("Objet proche détecté : ", nearby_object.name)
-
-func _on_area_exited(area):
-	# Vérifie si l'objet qui sort est l'objet interactif
-	if area.is_in_group("interactable") and area.get_parent() == nearby_object:
-		nearby_object = null  # Réinitialise l'objet proche
-		print("Objet n'est plus proche")
-
 func pick_up_object(object):
-	# Logique pour ramasser l'objet
 	print("Objet ramassé : ", object.name)
-	object.queue_free()  # Supprime l'objet de la scène
-	nearby_object = null  # Réinitialise l'objet proche
-
-
-func _on_area_3d_area_entered(area):
-	pass # Replace with function body.
+	object.queue_free()
+	nearby_object = null
