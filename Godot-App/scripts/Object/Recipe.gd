33fd81229eef66bdbdf6708d5cc32988c7d93384
Nantenaina isaac <ujesaisnantah@gmail.com>
2025-02-03 13:36:38 +0300

    2025

---
+++ Godot-App/scripts/Object/Recipe.gd
@@ -0,0 +1,6 @@
+class_name Recipe
+extends Resource
+
+@export var name:String = "Four"
+@export var ingredients:Array[Item] = []
+@export var results:Array[Item] = []
--- Godot-App/scripts/player/player.gd
+++ Godot-App/scripts/player/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 const SPEED = 2.0
-const JUMP_VELOCITY = 4.5
 const ROTATION_SPEED = 10.0
 
 @onready var camera_point = $camera_point
@@ -22,15 +21,12 @@ var cane_take_object = false
 
 func _ready():
 	GameManager.set_player(self)
-	animation_player.set_blend_time("idle", "walk", 0.2)
-	animation_player.set_blend_time("walk", "idle", 0.2)
+	animation_player.set_blend_time("idle", "walk", 0.3)
+	animation_player.set_blend_time("walk", "idle", 0.3)
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
